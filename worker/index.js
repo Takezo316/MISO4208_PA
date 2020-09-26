f0a33393ccfacf740102cb78f447fe1305d25db3
Jhonnyguzz <jhjguzmanri@unal.edu.co>
2020-09-26 03:53:47 -0500

    Express and S3

--- worker/index.js
+++ worker/index.js
@@ -5,11 +5,38 @@ const port = 8080;
 const cypress = require("cypress");
 const AWS = require("aws-sdk");
 
+const path = require('path')
+const fs = require('fs')
+
 const s3 = new AWS.S3({
-    region: process.env.AWS_REGION,
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
+    region: 'HERE',
+    accessKeyId: 'HERE',
+    secretAccessKey: 'HERE',
 });
+
+const bucket = 'habiticamiso'
+
+let uploadToS3 = (videoPath) => { 
+    let fileStream = fs.createReadStream(videoPath)
+    fileStream.on('error', function (err) {
+        console.log('File Error', err);
+    })
+
+    let params = {
+        Bucket: bucket,
+        Key: path.relative('.', videoPath),
+        Body: fileStream
+    }
+
+    s3.upload(params, function (err, data) {
+        if (err) {
+            console.log("Unable to upload to S3")
+            console.log(err)
+        }else
+            console.log("Upload Success", data.Location)
+    })
+
+}
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
@@ -23,6 +50,7 @@ app.post("/test/login", (req, res) => {
         .then((results) => {
             console.log(results);
             console.log("Fin de los resultados");
+            uploadToS3(results.runs[0].video)
             res.status(200).send("Subiendo resultados a S3")
         })
         .catch((err) => {
