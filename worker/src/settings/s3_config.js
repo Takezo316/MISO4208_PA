b73b35b9fce152131d964d94c80f02128eee5429
Jhonnyguzz <jhjguzmanri@unal.edu.co>
2020-10-24 02:16:45 -0500

    Identing space

--- worker/src/settings/s3_config.js
+++ worker/src/settings/s3_config.js
@@ -1,9 +1,9 @@
 const AWS = require("aws-sdk");
 
 const s3 = new AWS.S3({
-    region: 'us-east-1',
-    accessKeyId: 'AKIA46QBY4USVR2LWSLA',
-    secretAccessKey: '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',
+    region: '',
+    accessKeyId: '',
+    secretAccessKey: '',
 })
 
 module.exports = s3
