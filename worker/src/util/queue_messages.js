3a05e79d6da9e5e8d5e1182438ef2880274508fc
Jhonnyguzz <jhjguzmanri@unal.edu.co>
2020-12-18 10:06:38 -0500

    Saving queue results to db POC

--- worker/src/util/queue_messages.js
+++ worker/src/util/queue_messages.js
@@ -1,4 +1,6 @@
-const sqs = require('../settings/sqs_config');
+const axios = require('axios')
+const sqs = require('../settings/sqs_config')
+const mysql = require('./save_db')
 
 const queueURL = "https://sqs.us-east-1.amazonaws.com/890135700773/automaticascola"
 const params = {
@@ -18,16 +20,44 @@ let sleep = (time) => {
     return new Promise((resolve) => {
         setTimeout(resolve, time)
     })
-} 
+}
 
 let processQueueMessage = async () => {
     sqs.receiveMessage(params, async function(err, data) {
-        if (err) {
+        if(err) {
             console.log("Receive Error", err)
-        } else if (data.Messages) {
-            //TODO
-            console.log(data.Messages[0].Body)
+        }else if(data.Messages) {
+            console.log("Message incoming")
+            let body = JSON.parse(data.Messages[0].Body)
+            let executionIndex = -1
+            mysql.query("INSERT INTO `mydb`.`executions` (`app_ver_id`, `test_feature_id`, `user_id`, `created_at`) VALUES (?, ?, ?, NOW())", ['1', '1', '1'], function(e, r) {
+                if(e)
+                    console.log("Error to insert execution", e)
+                else {
+                    executionIndex = r.insertId
+                    mysql.query("INSERT INTO executions_status (`execution_id`, `status`, `result`, `details`, `s3_url`) VALUES (?, 'PENDING', 'null', 'null', 'null')", [r.insertId], function(e1, r1) {
+                        if(e1)
+                            console.log("Error to insert execution_status", e1)
+                    })
 
+                    axios.post(body.path).then(res => {
+                        //console.log(res.data)
+        
+                        let sql = "UPDATE `mydb`.`executions_status` SET `status` = 'PASSED', `result` = 'TODO', `details` = 'TODO', `s3_url` = 'TODO' WHERE (`id` = ?)"
+                        mysql.query(sql, [executionIndex], function(err3, result) {
+                            if(err3)
+                                console.log("Error to update execution_status")
+                            console.log(result)
+                        })
+        
+                    }).catch(fatal => {
+                        //Save error to db
+                        console.log("Error", fatal)
+                    })
+                }
+            })
+
+            //Delete message
             let deleteParams = {
                 QueueUrl: queueURL,
                 ReceiptHandle: data.Messages[0].ReceiptHandle
